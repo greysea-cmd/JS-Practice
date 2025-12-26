3e27b0dfe2f560201596cddf886e785a4a02b9a8
greysea-cmd <blissss2001@gmail.com>
2025-12-26 16:28:12 +0545

    Addition

--- learning/practice.js
+++ learning/practice.js
@@ -5,5 +5,7 @@ console.log("Addition: ", a + b);
 console.log("Subtraction: ", a - b);
 console.log("Multiplication: ", a * b);
 console.log("Division: ", a / b);
-console.log("Modulus: ", a % b);
+console.log("Remainder: ", a % b);
 console.log("Exponentiation: ", a ** b);
+console.log("Increment: ", ++a);
+console.log("Decrement: ", --b);
