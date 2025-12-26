538128ff0c6badc49a550f97e2cbe6c36ef244e7
greysea-cmd <blissss2001@gmail.com>
2025-12-26 16:39:09 +0545

    Assignment

---
+++ learning/assignment_operators.js
@@ -0,0 +1,9 @@
+//assignment operators
+let a=8;
+console.log("a=",a);
+console.log("a += 5:", a += 5); // addition assignment
+console.log("a -= 3:", a -= 3); // subtraction assignment
+console.log("a *= 2:", a *= 2); // multiplication assignment
+console.log("a /= 4:", a /= 4); // division assignment
+console.log("a %= 3:", a %= 3); // remainder assignment
+console.log("a **= 2:", a **= 2); // exponentiation assignment...confusion
