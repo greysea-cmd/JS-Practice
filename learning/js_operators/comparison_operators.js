5d95fa7c8ef24407d8fc3c2a531b56f397dc4c33
greysea-cmd <blissss2001@gmail.com>
2025-12-26 16:46:55 +0545

    comparison

--- learning/js_operators/comparison_operators.js
+++ learning/js_operators/comparison_operators.js
@@ -3,3 +3,9 @@ let x = 10;
 let y = 20;
 console.log("x>y:", x > y); // greater than
 console.log("x<y:", x < y); // less than
+console.log("x>=y:", x >= y); // greater than or equal to
+console.log("x<=y:", x <= y); // less than or equal to
+console.log("x==y:", x == y); // equal to
+console.log("x!=y:", x != y); // not equal to
+console.log("x===y:", x === y); // strict equal to
+console.log("x!==y:", x !== y); // strict not equal to
